c6be5982fdacbfbd02ed1682c2a6cd4398439f59
LUISGERARDO11 <luis34julieta@gmail.com>
2024-02-10 17:48:34 -0600

    actualizacion

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // Al cargar la página, ocultamos las cortinas
+    $('.left-curtain').css('width', '0%');
+    $('.right-curtain').css('width', '0%');
+  
+  });
+  
